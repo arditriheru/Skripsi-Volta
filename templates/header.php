aa0ee6964af01719ed9447a3e225dc37f6d765c3
arditriheru <arditriheruh@gmail.com>
2021-10-24 14:19:00 +0700

    tambah halaman treatment

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Login - Beauty Lux</title>
+    <title>Beauty Lux - Acne & Glowing Center</title>
 </head>
 
 <body>
